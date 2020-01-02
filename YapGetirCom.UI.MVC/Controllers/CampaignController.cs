c5aaa0a073360dc9be9d5485f7bd1da50bdda5a4
ÜMİT SONAR <sonar.umit@gmail.com>
2020-01-02 12:51:23 +0300

    kampanya değişiklikleri

--- YapGetirCom.UI.MVC/Controllers/CampaignController.cs
+++ YapGetirCom.UI.MVC/Controllers/CampaignController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YapGetirCom.BLL.Abstract;
+using YapGetirCom.Model;
 
 namespace YapGetirCom.UI.MVC.Controllers
 {
     public class CampaignController : Controller
     {
         // GET: Campaign
+        private ICampaignService _campaignService;
+
+        public CampaignController(ICampaignService campaignService)
+        {
+            _campaignService = campaignService;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Campaign> campaigns = _campaignService.GetAll().ToList();
+            return View(campaigns);
         }
     }
 }
